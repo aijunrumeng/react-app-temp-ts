96ee291b94ada7c413f5e8514c10e5c110964c94
liangchen <15050558187@163.com>
2022-07-06 11:38:04 +0800

    router example

--- src/pages/home/Home.tsx
+++
@@ -1,5 +0,0 @@
-import Styles from './home.less';
-
-export default function Home() {
-  return <div className={Styles['home']}>home</div>;
-}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 
-import Home from '@/pages/home';
+import Home from '@/pages/Home';
+import Foo from '@/pages/Foo';
+import Bar from '@/pages/Bar';
 
 import NotFound from '@/pages/404';
 
@@ -9,9 +11,11 @@ export default function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/foo" element={<Foo />}></Route>
+        <Route path="/bar" element={<Bar />}></Route>
         {/* other routes */}
-        <Route path='*' element={<NotFound />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
